0fb0904ef6f77419279cf9fa230057e2435cf4ed
Alexis Weber <alexis.weber@mailfence.com>
2023-11-18 22:01:17 -0300

    first commit southsolutionsweb

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,134 @@
-import Image from 'next/image'
+import Image from "next/image";
+import movil from "../public/movil.jpeg";
+import pc from "../public/pc.jpeg";
+import SouthSolutions4 from "../public/SouthSolutions4.jpg";
+import SouthSolutions5 from "../public/SouthSolutions5.jpg";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  
+  const back1 = "#d4f2fc"
+  const back2 ="#edf5f7"
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  const prueba1 = () => {
+    return (
+      <div>
+      <div
+        className="flex justify-center"
+        style={{ backgroundColor: back1 }}>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={SouthSolutions4}
+          alt="SouthSolutions"
+          width={200}
+          height={200}
         />
+        <h1 className="text-center text-5xl p-6 justify-center items-center m-9">
+          SouthSolutions
+          <h2 className="font-mono text-center text-3xl mt-5">
+            tu propia app, tu propia web
+          </h2>
+        </h1>
       </div>
+      <hr />
+    </div>
+    )
+  }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+  const prueba2 = () => {
+    return(
+      <div>
+      <div
+        className="flex justify-center"
+        style={{ backgroundColor: back2 }}>
+        <Image
+          src={SouthSolutions5}
+          alt="SouthSolutions"
+          width={300}
+          height={300}
+        />
+        <h1 className="text-center text-5xl p-6 justify-center items-center m-9">
+          SouthSolutions
+          <h2 className="font-mono text-center text-3xl mt-5">
+            tu propia app, tu propia web
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </h1>
       </div>
+      <hr />
+    </div>
+    )
+  }
+  return (
+    <main>
+      <section>
+       {prueba1()}
+      </section>
+
+      <section>
+        <h3 className="text-center mt-10 text-2xl underline">
+          ¿Quienes somos?
+        </h3>
+
+        <article className="text-center p-5">
+          Somos un grupo de desarrolladores web y mobile enfocados en agregar
+          valor a cada producto, con responsabilidad y dedicación para
+          satisfacer las necesidades de nuestros clientes. creamos productos de
+          calidad enfocado a los mercados digitales de toda Latinoamérica
+        </article>
+      </section>
+
+      <section>
+        <h3 className="text-center mt-10 text-2xl ">
+          ¿Por qué tener tu propia app?
+        </h3>
+        <div className="flex justify-between p-4 mx-5 items-center w-4/5">
+          <article className="text-center p-6 ">
+            En un mundo cada vez más digital, tener una aplicación móvil para tu
+            negocio es esencial. Una aplicación móvil te permite llegar a los
+            usuarios dondequiera que estén, conectando con tu público de manera
+            directa, a un simple <dfn>tap</dfn> de distancia. Imagina que tu
+            negocio esté en el bolsillo de tus clientes, disponible 24/7,
+            ofreciendo una experiencia personalizada y optimizada para sus
+            necesidades. Con nuestras aplicaciones, puedes expandir tu negocio,
+            aumentar tu alcance y ofrecer un servicio excepcional, todo al
+            alcance de la mano de tus clientes.
+          </article>
+
+          <Image
+            src={movil}
+            alt="movil"
+            width={200}
+            height={100}
+            style={{ borderRadius: 99999 }}
+          />
+        </div>
+      </section>
+
+      <section>
+        <h3 className="text-center mt-10 text-2xl">
+          ¿Por qué tener tu propia web?
+        </h3>
+        <div className="flex justify-between p-4 mx-5 items-center w-4/5">
+          <Image
+            src={pc}
+            alt="web"
+            width={200}
+            height={100}
+            style={{ borderRadius: 99999 }}
+          />
+          <article className="text-center p-6">
+            En la era digital, tener una página web es como tener una tarjeta de
+            presentación para el mundo. Una página web bien diseñada puede ser
+            la diferencia entre un cliente potencial y un cliente perdido. Tu
+            página web es el primer punto de contacto para muchos de tus
+            clientes, y queremos asegurarnos de que esa primera impresión sea la
+            mejor. Con nuestras soluciones de desarrollo web, puedes tener una
+            página web atractiva, fácil de usar y optimizada para motores de
+            búsqueda. Así, tus clientes pueden encontrarte fácilmente, aprender
+            más sobre tus servicios y contactarte con solo unos pocos clics.
+          </article>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
