ebabd33e440a5ee92cd64140a54d7838ef79b9c1
Alexis Weber <alexis.weber@mailfence.com>
2023-11-21 17:43:53 -0300

    add project route

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Card from "../components/Card/Card";
+
+const Projects = () => {
+  return (
+    <section>
+      
+      <h1>conoce los proyectos en los que trabajan nuestros desarrolladores</h1>
+    </section>
+  );
+};
+
+export default Projects;
