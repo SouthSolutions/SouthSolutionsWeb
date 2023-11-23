a0f676d5a55eaa08c27e536e83ca5b5ed0eb21bd
Alexis Weber <alexis.weber@mailfence.com>
2023-11-23 15:50:33 -0300

    create project component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,10 @@
 import Card from "../components/Card/Card";
+import Nav from "../components/Nav/Nav";
 
 const Projects = () => {
   return (
     <section>
-      
+      <Nav/>
       <h1>conoce los proyectos en los que trabajan nuestros desarrolladores</h1>
     </section>
   );
