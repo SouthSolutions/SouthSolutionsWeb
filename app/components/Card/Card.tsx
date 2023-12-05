db4b82cb81809645030f404ec2bd324be97ab623
Alexis Weber <alexis.weber@mailfence.com>
2023-12-05 13:59:55 -0300

    change image size and responsive params

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -19,7 +19,7 @@ const Card: FC<CardProps> = ({ image, description, name, url, platform }) => {
   const handleHover = () => {
     if (isHovered) {
       return (
-        <div className="group transition-all duration-[1s]">
+        <div className="group transition-all duration-[1.3s] text-[.6rem] md:text-md lg:text-[1rem]">
           <p>{description}</p>
         </div>
       );
@@ -28,26 +28,25 @@ const Card: FC<CardProps> = ({ image, description, name, url, platform }) => {
 
   return (
     <main>
-      <section className="flex p-2">
+      <section className="flex p-1">
         <Link
           href={url}
-          className="  group group-hover:p-[2.5em] transition-all duration-[1s] ease-in-out">
-          <article className="border rounded-[3em] text-center  ease-in-out mx-auto w-[15em] transition-all duration-[1s]">
+          className="  group group-hover:p-[2rem] transition-all duration-[1s] ease-in-out">
+          <article className="border rounded-[3em] text-center  ease-in-out mx-auto w-[8rem] transition-all duration-[1s] md:w-[10rem] lg:w-[13rem]">
             <figure title={description} className="mx-auto">
-              <figcaption>{name}</figcaption>
+              <figcaption className=" text-[.76rem] md:text-[.9rem] lg:text-lg">{name}</figcaption>
               <Image
-                className="rounded-[3em] mx-auto p-4 group-hover:p-[2.5em] transition-all duration-[1s] ease-in-out"
+                className="rounded-[3em] mx-auto p-2 group-hover:p-[1.5em] transition-all duration-[1s] ease-in-out md:p-3 lg:p-5"
                 src={image}
                 alt={name}
-                height={200}
-                width={200}
+                height={160}
+                width={160}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
               />
             </figure>
             <ul>
-              <li className="group-hover:p-[2.5em] text-[.7em] transition-all duration-[1s] ease-in-out ">
-                {" "}
+              <li className="group-hover:p-[1em] text-[.7em] transition-all duration-[1s] ease-in-out ">
                 {handleHover()}
               </li>
               <li>{platform}</li>
