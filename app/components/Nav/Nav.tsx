23e1d75476435cfd2f5b0b8693730017cff774e2
Alexis Weber <alexis.weber@mailfence.com>
2023-12-05 15:40:02 -0300

    change spaces on options and images, change size text responsive

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -5,38 +5,43 @@ import SouthSolutionstext from "../../../public/SouthSolutionstext.jpg";
 
 const Nav = () => {
   return (
-    <section className="flex justify-between" style={{ backgroundColor: "#edf5f7" }}>
-    <div className="justify-between flex bg-[#edf5f7]">
-      <Image
-        className="m-1.5 rounded-[50%] bg-[#edf5f7] "
-        src={SouthSolutions5}
-        alt="SouthSolutionsLogo"
-        width={90}
-        height={10}
-      />
-      <Link href="/"> 
-      <Image
-        className="ml-sm rounded-[1em] p-6 w-full md:w-auto bg-[#edf5f7]  "
-        src={SouthSolutionstext}
-        alt="SouthSolutionsName"
-        height={100}
-        width={300} 
-      />
-      </Link>
-     
-    </div>
-  
-    <div className="flex flex-row items-start" style={{ marginTop: "10px" }}> 
-      <Link className="text-center mx-2 text-1xl p-2  hover:bg-[#2babae] hover:border rounded-[3em] hover:transition-all duration-300 ease-in-out" href="/projects">
-        Nuestros Proyectos
-      </Link>
-      <Link className="text-center mx-2 text-1xl p-2 hover:bg-[#2babae] hover:transition-all duration-300 ease-in-out hover:border rounded-[3em]" href="/contact">
-        Contactanos
-      </Link>
-    </div>
-  </section>
+    <section
+      className="flex justify-between p-3"
+      style={{ backgroundColor: "#edf5f7" }}>
+      <div className="justify-between flex bg-[#edf5f7]">
+        <Link href="/">
+          <Image
+            className="m-1 rounded-[50%] bg-[#edf5f7]  "
+            src={SouthSolutions5}
+            alt="SouthSolutionsLogo"
+            width={60}
+            height={10}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            className="mx-2 rounded-[1em] p-2 w-full bg-[#edf5f7]  "
+            src={SouthSolutionstext}
+            alt="SouthSolutionsName"
+            height={100}
+            width={300}
+          />
+        </Link>
+      </div>
 
-  
+      <div className="flex flex-row items-start" style={{ marginTop: "10px" }}>
+        <Link
+          className="text-center ml-[1rem] lg:mx-2 text-[.6rem] md:text-[.9rem] lg:text-[1.1rem] p-2  hover:bg-[#2babae] hover:border rounded-[3em] hover:transition-all duration-300 ease-in-out"
+          href="/projects">
+          Nuestros Proyectos
+        </Link>
+        <Link
+          className="text-center mx-.6 lg:mx-2 text-[.6rem] md:text-[.9rem] lg:text-[1.1rem] p-2 hover:bg-[#2babae] hover:transition-all duration-300 ease-in-out hover:border rounded-[3em]"
+          href="/contact">
+          Contactanos
+        </Link>
+      </div>
+    </section>
   );
 };
 
