ab8669be3420cbe8236733788e1415c32e5801ad
Alexis Weber <alexis.weber@mailfence.com>
2023-12-06 18:16:13 -0300

    add description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SouthSolutions | app's & web's",
-  description: "creamos aplicaciones web y moviles",
+  title: "SouthSolutions | tu app, tu web",
+  description: "En nuestro sitio web, ofrecemos servicios de creación de aplicaciones móviles y páginas web personalizadas para ayudar a nuestros clientes a llevar sus ideas al siguiente nivel. Desde aplicaciones móviles para iOS y Android hasta sitios web de comercio electrónico y blogs, nuestro equipo de expertos puede ayudarte a crear la presencia en línea que necesitas para tener éxito. ¡Contáctanos hoy para obtener más información!",
 };
 
 export default function RootLayout({
