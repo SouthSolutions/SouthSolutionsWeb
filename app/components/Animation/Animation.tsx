84ecc703d3aafade183979fad22b09ab03cd318f
Alexis Weber <alexis.weber@mailfence.com>
2023-12-06 16:45:26 -0300

    responsive animation, reduce p and text size

--- app/components/Animation/Animation.tsx
+++ app/components/Animation/Animation.tsx
@@ -21,11 +21,11 @@ export default function Animation() {
   return (
     <section>
       <div
-        className="p-40 flex justify-center m-6 bg-[linear-gradient(40deg,_#2babae_,_#edf5f7_,_#0f1026_,_#1b2040_,_#0f1026_,_#edf5f7_,_#2babae_)]"
+        className="py-[6.5rem] md:p-30 lg:p-40 flex justify-center m-3 bg-[linear-gradient(40deg,_#2babae_,_#edf5f7_,_#0f1026_,_#1b2040_,_#0f1026_,_#edf5f7_,_#2babae_)]"
         style={{ backgroundColor: "#edf5f7" }}>
         <h1
           style={{ fontFamily: "monospace" }}
-          className="text-4xl text-white"
+          className="text-3xl md:text-4xl lg:text-4xl text-white"
           id="typed"></h1>
       </div>
     </section>
