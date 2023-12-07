9065a3387f7f4f0aa411f8aa7f10a2e86308bc34
Alexis Weber <alexis.weber@mailfence.com>
2023-12-07 19:32:46 -0300

    reduce p to 1 for responsive

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -7,7 +7,7 @@ interface HamburgerMenuProps {
 
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ onClick }) => {
   return (
-    <div className="cursor-pointer p.2 md:p-3 lg:p-4 transition duration-300 ease-in-out transform hover:scale-110" onClick={onClick}>
+    <div className="cursor-pointer p-1 md:p-3 lg:p-4 transition duration-300 ease-in-out transform hover:scale-110" onClick={onClick}>
       <FiMenu size={35} />
     </div>
   );
