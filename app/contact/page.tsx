adb35d7ea6cd2d90aaa284c39a3ec98483e74a45
Alexis Weber <alexis.weber@mailfence.com>
2023-12-07 21:50:56 -0300

    repair responsive styles on contact options

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -18,44 +18,44 @@ const Contact = () => {
           <Nav />
         </nav>
         <h1 className="text-center text-3xl p-6">Contactanos!</h1>
-        <div className="flex justify-between flex-wrap items-center w-[20rem] md:w-[40rem] lg:w-[50rem] m-auto p-1 md:p-3 lg:p-[6rem]">
-          <Link href="https://wa.me/+5688575861?text=Hola!,%20vi%20su%20web,%20SouthSolutions%20y%20tengo%20una%20consulta">
+        <div className="flex justify-between flex-wrap items-center w-[20rem] md:w-[40rem] lg:w-[50rem] m-auto p-1 md:p-3 lg:p-[3rem]">
+          <Link href="https://wa.me/+5688575861?text=Hola!,%20vi%20su%20web,%20SouthSolutions%20y%20tengo%20una%20consulta" className="p-1 md:-4 lgp-6">
             <figure>
               <Image
                 src={wppImage}
-                width={150}
-                height={150}
+                width={140}
+                height={140}
                 alt="Whatsapp Franco"
               />
 
               <figcaption className="text-center">Franco</figcaption>
             </figure>
           </Link>
-          <Link href="https://wa.me/+541135655412?text=Hola!,%20vi%20su%20web,%20SouthSolutions%20y%20tengo%20una%20consulta">
+          <Link href="https://wa.me/+541135655412?text=Hola!,%20vi%20su%20web,%20SouthSolutions%20y%20tengo%20una%20consulta" className="p-1 md:p-4 lg:p-6">
             <figure>
               <Image
                 src={wppImage}
-                width={150}
-                height={150}
+                width={140}
+                height={140}
                 alt="Whatsapp Alexis"
               />
               <figcaption className="text-center">Alexis</figcaption>
             </figure>
           </Link>
-          <Link href="https://wa.me/+573156226982?text=Hola!,%20vi%20su%20web,%20SouthSolutions%20y%20tengo%20una%20consulta">
+          <Link href="https://wa.me/+573156226982?text=Hola!,%20vi%20su%20web,%20SouthSolutions%20y%20tengo%20una%20consulta" className="p-1 md:p-4 lg:p-6">
             <figure>
               <Image
                 src={wppImage}
                 alt="Whatsapp Wilson"
-                width={150}
-                height={150}
+                width={140}
+                height={140}
               />
               <figcaption className="text-center">Wilson</figcaption>
             </figure>
           </Link>
-          <Link href="mailto:southsolutions@gmail.com?subject=Consulta-contacto&body=Hola">
+          <Link href="mailto:southsolutions@gmail.com?subject=Consulta-contacto&body=Hola" className="p-1 md:p-4 lg:p-6">
             <figure>
-              <Image src={mailImage} alt="E-mail" width={150} height={150} className="p-[1.2rem]" />
+              <Image src={mailImage} alt="E-mail" width={130} height={130} className="p-[1.2rem]" />
 
               <figcaption className="text-center">SouthSolutions.tech</figcaption>
             </figure>
