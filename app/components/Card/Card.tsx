59fed8cafa62447d105ee7e1d06eda8a285bd9ab
Alexis Weber <alexis.weber@mailfence.com>
2023-12-08 22:30:00 -0300

    change hover effect on section (not on img) and chan w on responsive

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -13,41 +13,32 @@ interface CardProps {
   platform: string;
 }
 
+
 const Card: FC<CardProps> = ({ image, description, name, url, platform }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const handleHover = () => {
-    if (isHovered) {
-      return (
-        <div className="group transition-all duration-[1.3s] text-[.75rem] md:text-md lg:text-[1rem]">
-          <p>{description}</p>
-        </div>
-      );
-    }
-  };
-
   return (
     <main>
-      <section className="flex p-1">
-        <Link
-          href={url}
-          className="  group group-hover:p-[2rem] transition-all duration-[1s] ease-in-out">
-          <article className="border rounded-[3em] text-center  ease-in-out mx-auto w-[8rem] transition-all duration-[1s] md:w-[10rem] lg:w-[13rem]">
+      <section
+        className="flex p-1 group transition-all ease-in-out duration-[1.3s] text-[.75rem] md:text-md lg:text-[1rem]"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <Link href={url} className="group-hover:p-[2rem] transition-all duration-[1s] ease-in-out">
+          <article className="border rounded-[3em] text-center ease-in-out mx-auto w-[10rem] transition-all duration-[1s] md:w-[12rem] lg:w-[15rem]">
             <figure title={description} className="mx-auto">
               <figcaption className=" text-[.8rem] p-3 md:text-[.9rem] lg:text-lg">{name}</figcaption>
               <Image
-                className="rounded-[3em] mx-auto p-1 group-hover:p-[1.5em] transition-all duration-[1s] ease-in-out md:p-3 lg:p-5"
+                className="rounded-[3em] mx-auto p-1 group-hover:p-[1.5em] transition-all duration-[1s] ease-in-out md:p-3 lg:p-1"
                 src={image}
                 alt={name}
                 height={160}
                 width={160}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
               />
             </figure>
             <ul>
-              <li className="group-hover:p-[1em] text-[.7em] transition-all duration-[1s] ease-in-out ">
-                {handleHover()}
+              <li className="group-hover:p-[.5em] text-[.7em] transition-all duration-[1s] ease-in-out">
+                {isHovered && <p>{description}</p>}
               </li>
               <li>{platform}</li>
             </ul>
@@ -57,5 +48,6 @@ const Card: FC<CardProps> = ({ image, description, name, url, platform }) => {
     </main>
   );
 };
+
 
 export default Card;
