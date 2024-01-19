89748de757314e22ec6c245dda62b81438cb1782
Alexis Weber <alexis.weber@mailfence.com>
2024-01-19 11:07:10 -0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SouthSolutions | tu app, tu web",
-  description: "En nuestro sitio web, ofrecemos servicios de creación de aplicaciones móviles y páginas web personalizadas para ayudar a nuestros clientes a llevar sus ideas al siguiente nivel. Desde aplicaciones móviles para iOS y Android hasta sitios web de comercio electrónico y blogs, nuestro equipo de expertos puede ayudarte a crear la presencia en línea que necesitas para tener éxito. ¡Contáctanos hoy para obtener más información!",
+  description:
+    "Somos South Solutions Tech. En nuestro sitio web, ofrecemos servicios de creación de aplicaciones móviles y páginas web personalizadas para ayudar a nuestros clientes a llevar sus ideas al siguiente nivel. Desde aplicaciones móviles para iOS y Android hasta sitios web de comercio electrónico y blogs, nuestro equipo de expertos puede ayudarte a crear la presencia en línea que necesitas para tener éxito. ¡Contáctanos hoy para obtener más información!",
+  authors: {
+    url: "https://www.alexiscode.vercel.app",
+    name: "Alexis Weber",
+  },
+  keywords: "SouthSolutions, South Solutions, desarrollo de aplicaciones, app, desarrollo web, servicios de TI, tecnología"
 };
 
 export default function RootLayout({
