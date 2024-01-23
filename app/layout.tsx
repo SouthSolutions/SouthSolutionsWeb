6f268f819d9e217dd712d6496c459de5ace94b77
Alexis Weber <alexis.weber@mailfence.com>
2024-01-23 16:34:07 -0300

    change ortography on meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SouthSolutions | tu app, tu web",
   description:
-    "Somos South Solutions Tech. En nuestro sitio web, ofrecemos servicios de creación de aplicaciones móviles y páginas web personalizadas para ayudar a nuestros clientes a llevar sus ideas al siguiente nivel. Desde aplicaciones móviles para iOS y Android hasta sitios web de comercio electrónico y blogs, nuestro equipo de expertos puede ayudarte a crear la presencia en línea que necesitas para tener éxito. ¡Contáctanos hoy para obtener más información! SouthSolutions",
+    "Somos South Solutions Tech, En nuestro sitio web, ofrecemos servicios de creación de aplicaciones móviles y páginas web personalizadas para ayudar a nuestros clientes a llevar sus ideas al siguiente nivel. Desde aplicaciones móviles para iOS y Android hasta sitios web de comercio electrónico y blogs, nuestro equipo de expertos puede ayudarte a crear la presencia en línea que necesitas para tener éxito. ¡Contáctanos hoy para obtener más información! SouthSolutions",
   authors: {
     url: "https://www.southsolutions.tech",
     name: "South Solutions Technologies",
