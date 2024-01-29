16636614921088f7f3e1f5ca13d23b1a3b3a5144
Alexis Weber <alexis.weber@mailfence.com>
2024-01-28 23:38:19 -0300

    change animations hr, animation speed

--- app/components/Animation/Animation.tsx
+++ app/components/Animation/Animation.tsx
@@ -8,10 +8,11 @@ export default function Animation() {
     if (window) {
       const options = {
         strings: ["Tu Propia App", "Tu Propia Web"],
-        typeSpeed: 200,
+        typeSpeed: 100,
         backDelay: 500,
-        backSpeed: 200,
+        backSpeed: 100,
         loop: true,
+        showCursosr: true,
       };
 
       const typed = new Typed("#typed", options);
@@ -23,14 +24,11 @@ export default function Animation() {
       <div
         className="py-[6.5rem] md:py-30 lg:p-40 flex-col justify-center flex-column m-3 bg-[linear-gradient(40deg,_#2babae_,_#edf5f7_,_#0f1026_,_#1b2040_,_#0f1026_,_#edf5f7_,_#2babae_)]"
         style={{ backgroundColor: "#edf5f7" }}>
-        <h1 className="text-3xl md:text-4xl lg:text-4xl text-white text-center" >
-          SouthSolutions Tech
-          <hr />
-        </h1>
-        <br />
+        <h1 className="text-3xl md:text-4xl lg:text-4xl text-white text-center">SouthSolutions Tech</h1>
+        <hr />
         <h2
           style={{ fontFamily: "monospace" }}
-          className="text-2xl md:text-3xl lg:text-3xl text-white text-center transition-all duration-[1s]"
+          className=" text-2xl  md:text-3xl lg:text-3xl text-white text-center transition-all duration-[1s] mt-6"
           id="typed"></h2>
       </div>
     </section>
