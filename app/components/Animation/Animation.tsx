87e8b045bf63bbf679ca6bad99eba0e70cb7fd17
Alexis Weber <alexis.weber@mailfence.com>
2024-01-29 12:10:00 -0300

    styles animation

--- app/components/Animation/Animation.tsx
+++ app/components/Animation/Animation.tsx
@@ -24,12 +24,16 @@ export default function Animation() {
       <div
         className="py-[6.5rem] md:py-30 lg:p-40 flex-col justify-center flex-column m-3 bg-[linear-gradient(40deg,_#2babae_,_#edf5f7_,_#0f1026_,_#1b2040_,_#0f1026_,_#edf5f7_,_#2babae_)]"
         style={{ backgroundColor: "#edf5f7" }}>
-        <h1 className="text-3xl md:text-4xl lg:text-4xl text-white text-center">SouthSolutions Tech</h1>
+        <h1 className="text-3xl md:text-4xl lg:text-4xl text-white text-center">
+          SouthSolutions Tech
+        </h1>
         <hr />
-        <h2
-          style={{ fontFamily: "monospace" }}
-          className=" text-2xl  md:text-3xl lg:text-3xl text-white text-center transition-all duration-[1s] mt-6"
-          id="typed"></h2>
+        <div className="flex min-w-full justify-center">
+          <h2
+            style={{ fontFamily: "monospace" }}
+            className=" text-2xl  md:text-3xl lg:text-3xl text-white  transition-all duration-[1s] mt-6"
+            id="typed"></h2>
+        </div>
       </div>
     </section>
   );
