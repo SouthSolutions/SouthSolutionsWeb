9cd693cc5dbc15be67ad3549bae552b5bbd67e0c
Alexis Weber <alexis.weber@mailfence.com>
2024-01-28 23:37:53 -0300

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,10 @@ import "./globals.css";
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
+const webTitle = "South Solutions | tu app, tu web" || "SouthSolutions tu app, tu web";
 
-export const metadata: Metadata = {
-  title: "SouthSolutions | tu app, tu web",
+ export const metadata: Metadata = {
+  title: webTitle,
   description:
     "Somos South Solutions Tech, En nuestro sitio web, ofrecemos servicios de creación de aplicaciones móviles y páginas web personalizadas para ayudar a nuestros clientes a llevar sus ideas al siguiente nivel. Desde aplicaciones móviles para iOS y Android hasta sitios web de comercio electrónico y blogs, nuestro equipo de expertos puede ayudarte a crear la presencia en línea que necesitas para tener éxito. ¡Contáctanos hoy para obtener más información! SouthSolutions",
   authors: {
