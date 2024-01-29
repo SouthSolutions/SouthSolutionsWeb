e9cfe2e218d3d46f3615d7502575b8e2ffeac80c
Alexis Weber <alexis.weber@mailfence.com>
2024-01-29 12:24:58 -0300

    styles on animation, delete cursor

--- app/components/Animation/Animation.tsx
+++ app/components/Animation/Animation.tsx
@@ -12,7 +12,7 @@ export default function Animation() {
         backDelay: 500,
         backSpeed: 100,
         loop: true,
-        showCursosr: true,
+        showCursosr: false,
       };
 
       const typed = new Typed("#typed", options);
@@ -22,13 +22,13 @@ export default function Animation() {
   return (
     <section>
       <div
-        className="py-[6.5rem] md:py-30 lg:p-40 flex-col justify-center flex-column m-3 bg-[linear-gradient(40deg,_#2babae_,_#edf5f7_,_#0f1026_,_#1b2040_,_#0f1026_,_#edf5f7_,_#2babae_)]"
+        className="py-[6.5rem] md:py-30 lg:p-40  flex-col justify-center items-center align-middle flex-column m-3 bg-[linear-gradient(40deg,_#2babae_,_#edf5f7_,_#0f1026_,_#1b2040_,_#0f1026_,_#edf5f7_,_#2babae_)]"
         style={{ backgroundColor: "#edf5f7" }}>
         <h1 className="text-3xl md:text-4xl lg:text-4xl text-white text-center">
           SouthSolutions Tech
         </h1>
         <hr />
-        <div className="flex min-w-full justify-center">
+        <div className="flex justify-center">
           <h2
             style={{ fontFamily: "monospace" }}
             className=" text-2xl  md:text-3xl lg:text-3xl text-white  transition-all duration-[1s] mt-6"
