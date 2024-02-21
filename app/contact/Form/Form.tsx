d52719f00d6885c8a568d4315d8c0e2b6b9b9554
Alexis Weber <alexis.weber@mailfence.com>
2024-02-21 14:19:38 -0300

    create form

--- app/contact/Form/Form.tsx
+++ app/contact/Form/Form.tsx
@@ -5,13 +5,18 @@ import { useState } from "react";
 const ContactForm = () => {
   const [formState, setFormState] = useState({
     nameLastname: "",
-    mail: "",
+    email: "",
     subjet: "",
     bodyMail: "",
   });
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    return;
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement> | any
+  ) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
   };
   return (
     <div className="flex m-3">
@@ -25,16 +30,44 @@ const ContactForm = () => {
             id="nameLastname"
             name="nameLastname"
             className="p-1 border-warm-gray-500 border-[1.5px]"
+            value={formState.nameLastname}
+            onChange={handleChange}
           />
           <label htmlFor="email" className="p-3">
             Correo Electrónico
           </label>
           <input
             className="P-1 border-warm-gray-500 border-[1.5px]"
-            type="mail"
-            id="mail"
-            name="name"
-            value={formState.mail}
+            type="email"
+            id="email"
+            name="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <label htmlFor="subjet" className="p-3">
+            Asunto
+          </label>
+          <input
+            className="P-1 border-warm-gray-500 border-[1.5px]"
+            type="text"
+            name="subjet"
+            id="subjet"
+            value={formState.subjet}
+            onChange={handleChange}
+          />
+
+          <label
+            htmlFor="bodyMail"
+            placeholder="Tu Mensaje aquí"
+            className="p-3">
+            Tu mensaje
+          </label>
+          <input
+            className="P-1 border-warm-gray-500 border-[1.5px] h-[10rem] w-[20rem]"
+            type="textarea"
+            name="bodyMail"
+            value={formState.bodyMail}
+            id="bodyMail"
             onChange={handleChange}
           />
         </section>
