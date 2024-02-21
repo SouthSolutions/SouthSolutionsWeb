373cf05cc7cc36876c6bad989f789bd0105580e9
Alexis Weber <alexis.weber@mailfence.com>
2024-02-21 17:41:46 -0300

    delete animation on cards

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -13,41 +13,38 @@ interface CardProps {
   platform: string;
 }
 
-
 const Card: FC<CardProps> = ({ image, description, name, url, platform }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <main>
-      <section
-        className="flex p-1 group transition-all ease-in-out duration-[1.3s] text-[.8rem] md:text-[.9rem] lg:text-[1rem]"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <Link href={url} className="group-hover:p-[2rem] transition-all duration-[1s] ease-in-out">
-          <article className="border rounded-[3em] text-center ease-in-out mx-auto w-[10rem] transition-all duration-[1s] md:w-[12rem] lg:w-[15rem]">
+    <>
+      <section className="flex p-1">
+        <Link href={url} className="">
+          <article className="border rounded-[2em] text-center w-[10rem] md:w-[12rem] lg:w-[20rem]">
+            <h3 className="borded rounded-[2rem]">{name}</h3>
+            <hr />
             <figure title={description} className="mx-auto">
-              <figcaption className=" text-[.8rem] p-3 md:text-[.9rem] lg:text-lg">{name}</figcaption>
               <Image
-                className="rounded-[3em] mx-auto p-1 group-hover:p-[1.5em] transition-all duration-[1s] ease-in-out md:p-3 lg:p-1"
+                className="rounded-[2em] mx-auto p-1 "
                 src={image}
                 alt={name}
-                height={160}
-                width={160}
+                height={200}
+                width={200}
               />
+              <figcaption className=" text-[.8rem] p-1 md:text-[.9rem] lg:text-lg">
+                {description}
+              </figcaption>
             </figure>
-            <ul>
-              <li className="group-hover:p-[.5em] text-[.7em] transition-all duration-[1s] ease-in-out">
+
+            {/* <li className="group-hover:p-[.5em] text-[.7em] transition-all duration-[1s] ease-in-out">
                 {isHovered && <p>{description}</p>}
-              </li>
-              <li>{platform}</li>
-            </ul>
+              </li> */}
+            <div>{platform}</div>
           </article>
         </Link>
       </section>
-    </main>
+    </>
   );
 };
-
 
 export default Card;
